c21fda62d7487ca9e187267e3e9b3fa5a827e1c8
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-07 19:02:03 +0530

    [Sanya] Add: Access and Modify Static Fields

---
+++ src/main/java/com/bridgelabz/intermediateProblems/ModifyStaticFields.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.intermediateProblems;
+
+import java.lang.reflect.Field;
+
+class Configuration {
+    private static String API_KEY = "123Api-keY";
+}
+
+public class ModifyStaticFields {
+
+    public static void main(String[] args) throws Exception {
+        Configuration configuration = new Configuration();
+        Class<?> cls = configuration.getClass();
+
+        Field field = cls.getDeclaredField("API_KEY");
+        field.setAccessible(true);
+        System.out.println("Original Key: " + field.get(configuration));
+
+        field.set(field,"456Api-keY");
+        System.out.println("Modified Key: " + field.get(configuration));
+    }
+}
