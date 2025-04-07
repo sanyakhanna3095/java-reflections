e152cdcd20a3066c6c8c26b55320d4b00af191a6
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-07 11:17:20 +0530

    [Sanya] Add: class MathOperations with multiple public methods

---
+++ src/main/java/com/bridgelabz/intermediateProblems/DynamicMethodInvocation.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.intermediateProblems;
+
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+class MathOperations{
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int subtract(int a, int b){
+        return a-b;
+    }
+    public int multiply(int a, int b){
+        return a*b;
+    }
+}
+
+public class DynamicMethodInvocation {
+    public static void main(String[] args) throws Exception {
+        MathOperations op=new MathOperations();
+
+        Scanner sc=new Scanner(System.in);
+        Class<?> cls = op.getClass();
+
+        System.out.print("Enter method name (add, subtract, multiply): ");
+        String s = sc.nextLine();
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        Method method = cls.getMethod(s, int.class, int.class);
+
+        // Invoke method on object with parameters
+        Object result = method.invoke(op, a, b);
+        System.out.println("Result: " + result);
+    }
+}
